1866c3c0a05f274172686fb72cedcac5aa39b5c0
Luis Cabrera <luisca@microsoft.com>
2022-01-20 14:25:33 -0800

    First basic language demo

---
+++ ai_services/language/dotnet/LanguageBasicDemo/LanguageWrapper.cs
@@ -0,0 +1,185 @@
+﻿using Oci.AilanguageService;
+using Oci.AilanguageService.Models;
+using Oci.AilanguageService.Requests;
+using Oci.Common;
+using Oci.Common.Auth;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LanguageBasicDemo
+{
+    internal class LanguageWrapper
+    {
+        private readonly AIServiceLanguageClient client;
+
+        private static readonly int maxRecordsInBatch = 100;
+        private static readonly int maxCharactersInBatch = 20000;
+        private static readonly int maxCharactersInRecord = 5000;
+
+        public LanguageWrapper()
+        {
+            // Create a default authentication provider that uses the DEFAULT
+            // profile in the configuration file.
+            // Refer to <see href="https://docs.cloud.oracle.com/en-us/iaas/Content/API/Concepts/sdkconfig.htm#SDK_and_CLI_Configuration_File>the public documentation</see> on how to prepare a configuration file. 
+            var provider = new ConfigFileAuthenticationDetailsProvider("DEFAULT");
+            client = new AIServiceLanguageClient(provider, new ClientConfiguration());
+        }
+
+
+        public async Task<DetectedLanguage> GetTextLanguageAsync(string text)
+        {
+            // Create a request and dependent object(s).
+            var batchDetectDominantLanguageDetails = new BatchDetectDominantLanguageDetails
+            {
+                Documents = new List<DominantLanguageDocument>
+                {
+                    new DominantLanguageDocument
+                    {
+                        Key = "doc-1",
+                        Text = text
+                    }
+                }
+            };
+
+            var batchDetectDominantLanguageRequest = new BatchDetectDominantLanguageRequest
+            {
+                BatchDetectDominantLanguageDetails = batchDetectDominantLanguageDetails,
+                OpcRequestId = "394ZXI6MCFAHTJX1ZFSX<unique_ID>"
+            };
+
+            var response = await client.BatchDetectDominantLanguage(batchDetectDominantLanguageRequest);
+
+            // Retrieve value from the response.
+            var documentsValue = response.BatchDetectDominantLanguageResult.Documents;
+
+            //Console.WriteLine(documentsValue[0].Languages[0].ToString());
+
+            return documentsValue[0].Languages[0];
+        }
+
+
+        public async Task<List<KeyPhrase>> GetKeyPhrasesAsync(string text)
+        {
+            // Create a request and dependent object(s).
+            var batchDetails = new BatchDetectLanguageKeyPhrasesDetails
+            {
+                Documents = new List<KeyPhraseDocument>
+                {
+                    new KeyPhraseDocument
+                    {
+                        Key = "doc-1",
+                        Text = text
+                    }
+                }
+            };
+
+            var request = new BatchDetectLanguageKeyPhrasesRequest
+            {
+                BatchDetectLanguageKeyPhrasesDetails = batchDetails,
+                OpcRequestId = "394ZXI6MCFAHTJX1ZFSX<unique_ID>"
+            };
+
+            var response = await client.BatchDetectLanguageKeyPhrases(request);
+
+            // Retrieve value from the response.
+            var documentsValue = response.BatchDetectLanguageKeyPhrasesResult.Documents;
+
+            return documentsValue[0].KeyPhrases;
+        }
+
+        public async Task<TextClassification> ClassifyAsync(string text)
+        {
+            // Create a request and dependent object(s).
+            var batchDetails = new BatchDetectLanguageTextClassificationDetails
+            {
+                Documents = new List<TextClassificationDocument>
+                {
+                    new TextClassificationDocument
+                    {
+                        Key = "doc-1",
+                        Text = text
+                    }
+                }
+            };
+
+            var request = new BatchDetectLanguageTextClassificationRequest
+            {
+                BatchDetectLanguageTextClassificationDetails = batchDetails,
+                OpcRequestId = "394ZXI6MCFAHTJX1ZFSX<unique_ID>"
+            };
+
+            var response = await client.BatchDetectLanguageTextClassification(request);
+
+            // Retrieve value from the response.
+            var documentsValue = response.BatchDetectLanguageTextClassificationResult.Documents;
+
+            return documentsValue[0].TextClassification[0];
+        }
+
+        public async Task<List<HierarchicalEntity>> GetEntitiesAsync(string text)
+        {
+            // Create a request and dependent object(s).
+            var batchDetails = new BatchDetectLanguageEntitiesDetails
+            {
+                Documents = new List<EntityDocument>
+                {
+                    new EntityDocument
+                    {
+                        Key = "doc-1",
+                        Text = text
+                    }
+                }
+            };
+
+            var request = new BatchDetectLanguageEntitiesRequest
+            {
+                BatchDetectLanguageEntitiesDetails = batchDetails,
+                OpcRequestId = "394ZXI6MCFAHTJX1ZFSX<unique_ID>"
+            };
+
+            var response = await client.BatchDetectLanguageEntities(request);
+
+            // Retrieve value from the response.
+            var documentsValue = response.BatchDetectLanguageEntitiesResult.Documents;
+
+            return documentsValue[0].Entities;
+        }
+
+
+        public async Task<SentimentDocumentResult> GetSentimentAsync(string text)
+        {
+            // Create a request and dependent object(s).
+            var batchDetails = new BatchDetectLanguageSentimentsDetails
+            {
+                Documents = new List<SentimentsDocument>
+                {
+                    new SentimentsDocument
+                    {
+                        Key = "doc-1",
+                        Text = text
+                    }
+                }
+            };
+
+            var request = new BatchDetectLanguageSentimentsRequest
+            {
+                BatchDetectLanguageSentimentsDetails = batchDetails,
+                OpcRequestId = "394ZXI6MCFAHTJX1ZFSX<unique_ID>",
+                Level = new List<BatchDetectLanguageSentimentsRequest.LevelEnum>()
+                {  
+                    BatchDetectLanguageSentimentsRequest.LevelEnum.Sentence, 
+                    BatchDetectLanguageSentimentsRequest.LevelEnum.Aspect
+                }
+
+            };
+
+            var response = await client.BatchDetectLanguageSentiments(request);
+
+            // Retrieve value from the response.
+            var result = response.BatchDetectLanguageSentimentsResult.Documents;
+
+            return result[0];
+        }
+
+    }
+}
---
+++ ai_services/language/dotnet/LanguageBasicDemo/Program.cs
@@ -0,0 +1,99 @@
+﻿/******************************************************************
+ * 
+ *   This sample demonstrates how to issue calls to the OCI Language
+ *   services from a .Net application.
+ *
+ *   Pre-requisites:
+ *   Make sure you have created a configuration file with your credentials.
+ *   See https://docs.oracle.com/en-us/iaas/Content/API/Concepts/sdkconfig.htm#SDK_and_CLI_Configuration_File
+ *   for detailed instructions.  The Language class constructor in Language.cs reads that configuration file.
+ *   
+ *   Packages required: (You can install using the Nuget package manager)
+ *     OCI.DotNetSDK.Common
+ *     OCI.DotNetSDK.Ailanguage
+ *     
+ *   Files:
+ *   LanguageWrapper.cs is a wrapper on top of the OCI Language APIs that simplifies the use of the OCI Language SDK.
+ *   If you want more control over the API you may want to use the OCI Language SDK instead of the wrapper, for
+ *   example if you want to use batching capabilities.
+ * 
+ * *****************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Oci.AilanguageService.Models;
+
+namespace LanguageBasicDemo
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            // The text to analyze.
+            string textToAnalyze = "On Tuesday Jan 20 I met Bob Smith at Central Park. I like that he is a very prepared professional that should " +
+                                   "be able to help me with my financial services. He is familiar with the latest IRS codes, which is a big plus. " +
+                                   "That said, the cost of the service is too expensive for our current needs.";
+
+
+            Console.WriteLine("Text to analyze: \n" + textToAnalyze +"\n");
+            LanguageWrapper langInstance = new LanguageWrapper();
+
+
+            // Detect Dominant Language
+            DetectedLanguage language = await langInstance.GetTextLanguageAsync(textToAnalyze);
+            Console.WriteLine("\nLanguage: \n\t" + language);
+
+            // Classify the text 
+            var textclass = await langInstance.ClassifyAsync(textToAnalyze);
+            Console.WriteLine("\nText classification: \n\t {0} ({1:P2}) ", textclass.Label, textclass.Score);
+
+            // Extract key-phrases
+            List<KeyPhrase> keyphrases = await langInstance.GetKeyPhrasesAsync(textToAnalyze);
+            if (keyphrases != null)
+            {
+                Console.WriteLine("\nKey phrases: ");
+                foreach (KeyPhrase keyphrase in keyphrases)
+                {
+                    Console.WriteLine("\t{0} ({1:P2}) ", keyphrase.Text, keyphrase.Score);
+                }
+            }
+            Console.WriteLine();
+
+            // Extract Named Entities
+            List<HierarchicalEntity> entities = await langInstance.GetEntitiesAsync(textToAnalyze);
+            if (entities != null)
+            {
+                Console.WriteLine("\nEntities: ");
+                foreach (HierarchicalEntity entity in entities)
+                {
+                    Console.WriteLine("\t{0} \tType: {1} ({1:P2})   \tOffset: {2}, \tLength: {3}", entity.Text, entity.Type, entity.Score, entity.Offset, entity.Length);
+                }
+            }
+
+            // Perform sentiment analysis
+            var sentimentResult = await langInstance.GetSentimentAsync(textToAnalyze);
+            Console.WriteLine("\nDocument sentiment: " + sentimentResult.DocumentSentiment);
+
+            if (sentimentResult.Aspects != null)
+            {
+                Console.WriteLine("\n\tAspects: ");
+                foreach (var aspect in sentimentResult.Aspects)
+                {
+                    Console.WriteLine("\t{0} \tType: {1} ({1:P2})   \tOffset: {2}, \tLength: {3}", aspect.Text, aspect.Sentiment, aspect.Offset, aspect.Length);
+                }
+            }
+
+            if (sentimentResult.Sentences != null)
+            {
+                Console.WriteLine("\n\tSentences: ");
+                foreach (var sentence in sentimentResult.Sentences)
+                {
+                    Console.WriteLine("\t{0} \tType: {1} ({1:P2}) ", sentence.Text, sentence.Sentiment);
+                }
+            }
+
+        }
+
+    }
+}
