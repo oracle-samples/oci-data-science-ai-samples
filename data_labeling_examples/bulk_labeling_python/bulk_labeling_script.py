9a6d1b224c0fbd1fcbd58fdcd4559b814b9bbd69
puneet <puneet.mittal@oracle.com>
2022-04-05 14:27:28 +0530

    added comments and validation for dataset id

--- data_labeling_examples/bulk_labeling_python/bulk_labeling_script.py
+++ data_labeling_examples/bulk_labeling_python/bulk_labeling_script.py
@@ -19,15 +19,15 @@ def init_dls_dp_client(config, service_endpoint, retry_strategy):
     return dls_client
 
 
-def dls_list_records():
-    config_file = oci.config.from_file(CONFIG_FILE_PATH, CONFIG_PROFILE)
-    retry_strategy = oci.retry.DEFAULT_RETRY_STRATEGY
+config_file = oci.config.from_file(CONFIG_FILE_PATH, CONFIG_PROFILE)
+retry_strategy = oci.retry.DEFAULT_RETRY_STRATEGY
+dls_dp_client = init_dls_dp_client(config_file, SERVICE_ENDPOINT_DP, retry_strategy)
 
-    dls_dp_client = init_dls_dp_client(config_file, SERVICE_ENDPOINT_DP, retry_strategy)
 
+def dls_list_records(compartment_id):
     try:
         # limit parameter has an implicit value of 10 if not included
-        anno_response = dls_dp_client.list_records(compartment_id=COMPARTMENT_ID, dataset_id=DATASET_ID,
+        anno_response = dls_dp_client.list_records(compartment_id=compartment_id, dataset_id=DATASET_ID,
                                                    is_labeled=False, limit=LIST_RECORDS_LIMIT)
     except Exception as error:
         anno_response = error
@@ -43,11 +43,7 @@ def dls_list_records():
     return names, ids, len(ids)
 
 
-def dls_create_annotation(label, record_id):
-    config_file = oci.config.from_file(CONFIG_FILE_PATH, CONFIG_PROFILE)
-    retry_strategy = oci.retry.DEFAULT_RETRY_STRATEGY
-
-    dls_dp_client = init_dls_dp_client(config_file, SERVICE_ENDPOINT_DP, retry_strategy)
+def dls_create_annotation(label, record_id, compartment_id):
     label_lst = []
     if isinstance(label, str):
         label_lst.append(label)
@@ -61,7 +57,7 @@ def dls_create_annotation(label, record_id):
     entity_type = "GENERIC"
     entity_obj = [GenericEntity(entity_type=entity_type, labels=labels_obj)]
 
-    create_annotation_details_obj = CreateAnnotationDetails(record_id=record_id, compartment_id=COMPARTMENT_ID,
+    create_annotation_details_obj = CreateAnnotationDetails(record_id=record_id, compartment_id=compartment_id,
                                                             entities=entity_obj)
 
     # api call
@@ -80,10 +76,10 @@ def letter_to_label(letter):
             return l
 
 
-def first_letter(name, record_id):
+def first_letter(name, record_id, compartment_id):
     label = letter_to_label(letter=name[0])
     if label:
-        dls_create_annotation(label=label, record_id=record_id)
+        dls_create_annotation(label=label, record_id=record_id, compartment_id=compartment_id)
     else:
         print("current time: " + str(datetime.datetime.now()))
         print("No label match for record " + str(name))
@@ -99,10 +95,10 @@ def match_to_label(name):
             return l
 
 
-def first_match(name, record_id):
+def first_match(name, record_id, compartment_id):
     label = match_to_label(name=name)
     if label:
-        dls_create_annotation(label=label, record_id=record_id)
+        dls_create_annotation(label=label, record_id=record_id, compartment_id=compartment_id)
     else:
         print("current time: " + str(datetime.datetime.now()))
         print("No label match for record " + str(name))
@@ -110,7 +106,7 @@ def first_match(name, record_id):
         print()
 
 
-def custom_label_match(name, record_id):
+def custom_label_match(name, record_id, compartment_id):
     regex_lst = list(LABEL_MAP.keys())
     label_var = ""
     for regex_exp in regex_lst:
@@ -119,29 +115,37 @@ def custom_label_match(name, record_id):
             break
     if label_var != "":
         label_lst = LABEL_MAP[label_var]
-        dls_create_annotation(label=label_lst, record_id=record_id)
+        dls_create_annotation(label=label_lst, record_id=record_id, compartment_id=compartment_id)
 
 
-def label_record(name, id, labeling_algorithm):
+def label_record(name, id, labeling_algorithm, compartment_id):
     if labeling_algorithm == "FIRST_REGEX_MATCH":
-        first_match(name=name, record_id=id)
+        first_match(name=name, record_id=id, compartment_id=compartment_id)
     elif labeling_algorithm == "FIRST_LETTER_MATCH":
-        first_letter(name=name, record_id=id)
+        first_letter(name=name, record_id=id, compartment_id=compartment_id)
     elif labeling_algorithm == "CUSTOM_LABELS_MATCH":
-        custom_label_match(name=name, record_id=id)
+        custom_label_match(name=name, record_id=id, compartment_id=compartment_id)
 
 
 def main():
     if __name__ == '__main__':
-        start = time.perf_counter()
-        num_records = LIST_RECORDS_LIMIT
-        pool = NO_OF_PROCESSORS
-        while num_records == LIST_RECORDS_LIMIT:
-            names, ids, num_records = dls_list_records()
-            pool.starmap(label_record, zip(names, ids, repeat(LABELING_ALGORITHM)))
-        pool.close()
-        end = time.perf_counter()
-        print(f'Finished in {round(end - start, 2)} second(s)')
+        try:
+            response = dls_dp_client.get_dataset(dataset_id=DATASET_ID)
+        except Exception as error:
+            response = error
+        if response.status == 200:
+            compartment_id = response.data.compartment_id
+            start = time.perf_counter()
+            num_records = LIST_RECORDS_LIMIT
+            pool = mp.Pool(NO_OF_PROCESSORS)
+            while num_records == LIST_RECORDS_LIMIT:
+                names, ids, num_records = dls_list_records(compartment_id=compartment_id)
+                pool.starmap(label_record, zip(names, ids, repeat(LABELING_ALGORITHM), repeat(compartment_id)))
+            pool.close()
+            end = time.perf_counter()
+            print(f'Finished in {round(end - start, 2)} second(s)')
+        else:
+            print(response)
 
 
 main()
--- data_labeling_examples/bulk_labeling_python/config.py
+++ data_labeling_examples/bulk_labeling_python/config.py
@@ -10,21 +10,20 @@ CONFIG_PROFILE = "DEFAULT"
 REGION_IDENTIFIER = "uk-london-1"
 # service_endpoint
 SERVICE_ENDPOINT_DP = f"https://dlstest-dp.{REGION_IDENTIFIER}.oci.oraclecloud.com"
-# compartment where DLS Dataset exists
-COMPARTMENT_ID = "ocid1.compartment.oc1..aaaaaaaamrnszmfhl7xzawtef3rl6gmsb62tcoag32yiv5ujorognkuzrqfq"
 # ocid of the DLS Dataset
-DATASET_ID = "ocid1.datalabelingdatasetint.oc1.uk-london-1.amaaaaaaniob46iarr2zttq7c5th3jfqwab7d3vrq4daa52tcnnwhkgrowca"
+DATASET_ID = "ocid1.datalabelingdatasetint.oc1.uk-london-1.amaaaaaaniob46ias33axzqmubpp5znqm7yl5kqvhunmrn7jxdl2pnjpd75q"
 # an array where the elements are all of the labels that you will use to annotate records in your DLS Dataset with.
 # Each element is a separate label.
 LABELS = ["dog", "cat"]
 # the algorithm that will be used to assign labels to DLS Dataset records
-LABELING_ALGORITHM = "FIRST_LETTER_MATCH"
+# Possible values for labeling algorithm "FIRST_LETTER_MATCH", "FIRST_REGEX_MATCH", "CUSTOM_LABELS_MATCH"
+LABELING_ALGORITHM = "CUSTOM_LABELS_MATCH"
 # use for first_match labeling algorithm
 FIRST_MATCH_REGEX_PATTERN = r'^([^/]*)/.*$'
 # maximum number of DLS Dataset records that can be retrieved from the list_records API operation for labeling
 # limit=1000 is the hard limit for list_records
 LIST_RECORDS_LIMIT = 1000
 # For CUSTOM_LABEL_MATCH specify the label map
-LABEL_MAP = {"dog/": ["dog", "tommy"], "cat/": ["cats", "meow"]}
-# the no of processes to be used for parallel execution by default is set to maximum no of processors int he system
-NO_OF_PROCESSORS = mp.Pool(mp.cpu_count())
+LABEL_MAP = {"dog/": ["dogs", "tommy"], "cat/": ["cats", "meow"]}
+# the no of processes to be used for parallel execution by default is set to maximum no of processors in the system
+NO_OF_PROCESSORS = mp.cpu_count()
