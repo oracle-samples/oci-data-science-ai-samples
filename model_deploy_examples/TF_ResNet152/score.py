2c87c95684346569fbedcb06ce3551ad649a9025
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-28 23:13:19 +0000

    Adding tarfile member sanitization to extractall()

--- model_deploy_examples/TF_ResNet152/score.py
+++ model_deploy_examples/TF_ResNet152/score.py
@@ -25,7 +25,26 @@ def load_model(model_file_name=model_name):
         with TemporaryDirectory() as tmpdir: 
             tmp_path = os.path.join(tmpdir, model_file_name)
             with tarfile.open(os.path.join(model_dir, model_file_name + '.tar.gz'), 'r') as tar:
-                tar.extractall(path=tmp_path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=tmp_path)
             model = tf.saved_model.load(tmp_path)
         return model
     else:
