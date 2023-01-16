6bade173bb09f674e5ac7b955dc7bb4aa95beefe
sahilkalra <sahil.kalra@oracle.com>
2023-01-17 04:49:22 +0530

    Updating AI Service Language Python sample
    Signed-off-by: sahilkalra <sahil.kalra@oracle.com>

--- ai_services/language/java/src/main/java/com/company/Main.java
+++ ai_services/language/java/src/main/java/com/company/Main.java
@@ -14,6 +14,21 @@ import java.util.Arrays;
 
 public class Main {
 
+    private static String compartmentId = "<Specify your COMPARTMENT_ID here>";
+    private static String modelName = "ailangaugemodel";
+    private static String bucket_name = "<Specify name of your training data bucket here>"
+    private static String namespace_name = "<Specify the namespace here>"
+    private static String object_name = "<Specify training file name here>"
+    private static String projectId;
+    private static String modelId;
+    private static String nerEndpointId;
+    private static String txtcEndpointId;
+    private static String entext = "The Indy Autonomous Challenge is the worlds first head-to-head, high speed autonomous race taking place at the Indianapolis Motor Speedway";
+    private static String spanishText = "Este es un texto en el idioma de mi madre, la mejor mamá del mundo.";
+    String languageCode = "en";  // for english
+
+    private static AIServiceLanguageClient client;
+
     public static void main(String[] args) {
         try {
             /* Step 1: Just reading a config file with my credentials so the application acts on my behalf */
@@ -24,12 +39,12 @@ public class Main {
                     new ConfigFileAuthenticationDetailsProvider(configFile);
 
             /* Step 2: Create a service client */
-            AIServiceLanguageClient client = AIServiceLanguageClient.builder().build(provider);
+            client = AIServiceLanguageClient.builder().build(provider);
 
-            /* Step 3: Sample of single record API */
+            /*Sample of single record API */
             DetectDominantLanguageDetails detectdominantLanguageDetails =
                     DetectDominantLanguageDetails.builder()
-                    .text("Este es un texto en el idioma de mi madre, la mejor mamá del mundo.").build();
+                            .text(spanishText).build();
 
             DetectDominantLanguageRequest detectDominantLanguageRequest = DetectDominantLanguageRequest.builder()
                     .detectDominantLanguageDetails(detectdominantLanguageDetails)
@@ -40,15 +55,15 @@ public class Main {
                     detectDominantLanguage(detectDominantLanguageRequest);
 
             System.out.println("Detected language: " +
-                            response.getDetectDominantLanguageResult().getLanguages().get(0).getName());
+                    response.getDetectDominantLanguageResult().getLanguages().get(0).getName());
 
 
-            /* Step 4: Sample using more efficient batch APIs */
+            /*Sample using more efficient batch APIs */
             BatchLanguageTranslationDetails batchLanguageTranslationDetails = BatchLanguageTranslationDetails.builder()
                     .targetLanguageCode("en")
                     .documents(new ArrayList<>(Arrays.asList(TextDocument.builder()
                             .key("key1")
-                            .text("El idioma español es muy facil de aprender.")
+                            .text(spanishText)
                             .languageCode("es").build()))).build();
 
             BatchLanguageTranslationRequest batchLanguageTranslationRequest = BatchLanguageTranslationRequest.builder()
@@ -59,10 +74,152 @@ public class Main {
             BatchLanguageTranslationResponse response1 = client.batchLanguageTranslation(batchLanguageTranslationRequest);
             System.out.println("Translation: " + response1.getBatchLanguageTranslationResult().getDocuments().get(0).getTranslatedText());
 
+            /* Custom Models */
+            /* Create an object */
+            Main aiServiceLanguageExample = new Main();
 
-        }
-        catch(IOException e) {
+            // Create AiLanguageProject
+            Project languageProject = aiServiceLanguageExample.createLanguageProject();
+            projectId = languageProject.getId();
+            System.out.println(languageProject.toString());
+
+            // wait till project state becomes ACTIVE
+            while (languageProject.getLifecycleState() == Project.LifecycleState.Creating){
+                System.out.println("Waiting for project creation to complete...");
+                Thread.sleep(4000);
+                languageProject = aiServiceLanguageExample.getLanguageProject(projectId);
+            }
+            languageProject = aiServiceLanguageExample.getLanguageProject(projectId);
+            System.out.println("Project status changed to" + languageProject.getLifecycleState());
+
+            /* Create and train Custom NER Model */
+            // Create and train Custom NER AilanguageModel
+            Model languageModel = aiServiceLanguageExample.createLanguageModel();
+            modelId =  languageModel.getId();
+            System.out.println(languageModel.toString());
+
+            // wait till model state becomes ACTIVE
+            while (languageModel.getLifecycleState() == Model.LifecycleState.Creating){
+                System.out.println("Waiting for model training to complete...");
+                Thread.sleep(60000);
+                languageModel = aiServiceLanguageExample.getLanguageModel(modelId);
+            }
+            languageModel = aiServiceLanguageExample.getLanguageModel(modelId);
+            System.out.println("Model status changed to" + languageModel.getLifecycleState());
+
+            System.out.println("Printing model evaluation results");
+            System.out.println(languageModel.getEvaluationResults());
+
+            // Create AiLanguageEndpoint
+            Endpoint languageEndpoint = aiServiceLanguageExample.createLanguageEndpoint();
+            nerEndpointId = languageEndpoint.getId();
+            System.out.println(languageEndpoint.toString());
+
+            // wait till endpoint state becomes ACTIVE
+            while (languageEndpoint.getLifecycleState() == Endpoint.LifecycleState.Creating){
+                System.out.println("Waiting for endpoint creation to complete...");
+                Thread.sleep(60000);
+                languageEndpoint = aiServiceLanguageExample.getLanguageEndpoint(nerEndpointId);
+            }
+            languageEndpoint = aiServiceLanguageExample.getLanguageEndpoint(nerEndpointId);
+            System.out.println("Endpoint status changed to" + languageEndpoint.getLifecycleState());
+
+            // Inferencing on Custom Named Entity recognition model
+            String customDetectLanguageEntitiesResponse = aiServiceLanguageExample.getBatchDetectLanguageEntities(text);
+            System.out.println(customDetectLanguageEntitiesResponse.toString());
+
+            /* Create and train Custom Text classification Model */
+
+            // Create and train Custom Text classification AilanguageModel
+            languageModel = aiServiceLanguageExample.createLanguageModel();
+            modelId =  languageModel.getId();
+            System.out.println(languageModel.toString());
+
+            // Create AiLanguageEndpoint
+            languageEndpoint = aiServiceLanguageExample.createLanguageEndpoint();
+            txtcEndpointId = languageEndpoint.getId();
+            System.out.println(languageEndpoint.toString());
+
+            // Inferencing on Custom Text classification model
+            String customDetectLanguageTextClassificationResponse = aiServiceLanguageExample.getBatchDetectLanguageTextClassification(text);
+            System.out.println(customDetectLanguageTextClassificationResponse.toString());
+
+            client.close();
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    // Create AiLanguageProject
+    private Project createLanguageProject() {
+        CreateProjectDetails projectDetails = CreateProjectDetails.builder().compartmentId(compartmentId).build();
+        CreateProjectRequest request = CreateProjectRequest.builder().createProjectDetails(projectDetails).build();
+        CreateProjectResponse response = client.createProject(request);
+        return response.getProject();
+    }
+
+    // Get AiLanguageProject
+    private Project getLanguageProject(String projectOcid) {
+        GetProjectRequest request = GetProjectRequest.builder().projectId(projectOcid).build();
+        GetProjectResponse response = client.getProject(request);
+        return response.getProject();
+    }
+
+    // Create AiLanguageModel
+    private Model createLanguageModel() {
+        ModelDetails modeldtls = NamedEntityRecognitionModelDetails.builder().languageCode("en").build();
+        java.util.List<String> trainingDataobjects = Arrays.asList(object_name);
+        LocationDetails locationDetails = ObjectListDataset.builder().bucketName(bucket_name).namespaceName(namespace_name).objectNames(trainingDataobjects).build();
+        DatasetDetails trainingDataset = ObjectStorageDataset.builder().locationDetails(locationDetails).build();
+
+        CreateModelDetails modelDetails = CreateModelDetails.builder()
+                .compartmentId(compartmentId).displayName(modelName).projectId(projectId)
+                .modelDetails(modeldtls).trainingDataset(trainingDataset).build();
+        CreateModelRequest request = CreateModelRequest.builder().createModelDetails(modelDetails).build();
+        CreateModelResponse response = client.createModel(request);
+        return response.getModel();
+    }
+
+    // Get AiLanguageModel
+    private Model getLanguageModel(String modelOcid) {
+        GetModelRequest request = GetModelRequest.builder().modelId(modelOcid).build();
+        GetModelResponse response = client.getModel(request);
+        return response.getModel();
+    }
+
+    // Create AiLanguageEndpoint
+    private Endpoint createLanguageEndpoint() {
+        CreateEndpointDetails endpointDetails = CreateEndpointDetails.builder().compartmentId(compartmentId).modelId(modelId).inferenceUnits(1).build();
+        CreateEndpointRequest request = CreateEndpointRequest.builder().createEndpointDetails(endpointDetails).build();
+        CreateEndpointResponse response = client.createEndpoint(request);
+        return response.getEndpoint();
+    }
+
+    // Get AiLanguageEndpoint
+    private Endpoint getLanguageEndpoint(String endpointOcid) {
+        GetEndpointRequest request = GetEndpointRequest.builder().endpointId(endpointOcid).build();
+        GetEndpointResponse response = client.getEndpoint(request);
+        return response.getEndpoint();
+    }
+
+    // Custom Named Entity Recognition
+    private String getBatchDetectLanguageEntities(String text) {
+        TextDocument textDocument = TextDocument.builder().text(text).languageCode("en").key("key1").build();
+        java.util.List<TextDocument> documents = Arrays.asList(textDocument);
+        BatchDetectLanguageEntitiesDetails details = BatchDetectLanguageEntitiesDetails.builder().endpointId(nerEndpointId).documents(documents).build();
+        BatchDetectLanguageEntitiesRequest request = BatchDetectLanguageEntitiesRequest.builder().batchDetectLanguageEntitiesDetails(details).build();
+        BatchDetectLanguageEntitiesResponse response = client.batchDetectLanguageEntities(request);
+        return response.toString();
+    }
+
+    // Custom Text Classification
+    private String getBatchDetectLanguageTextClassification(String text) {
+        TextDocument textDocument = TextDocument.builder().text(text).languageCode("en").key("key1").build();
+        java.util.List<TextDocument> documents = Arrays.asList(textDocument);
+        BatchDetectLanguageTextClassificationDetails details = BatchDetectLanguageTextClassificationDetails.builder().endpointId(txtcEndpointId).documents(documents).build();
+        BatchDetectLanguageTextClassificationRequest request = BatchDetectLanguageTextClassificationRequest.builder().batchDetectLanguageTextClassificationDetails(details).build();
+        BatchDetectLanguageTextClassificationResponse response = client.batchDetectLanguageTextClassification(request);
+        return response.toString();
     }
 }
