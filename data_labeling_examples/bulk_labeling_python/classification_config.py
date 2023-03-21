49e9ef366f47b4c7451439bb1c8ce385e5ea1e66
Mahesh Kumar <mahesh.b.kumar@oracle.com>
2023-03-21 14:43:15 +0530

    Added Remove label functionality and updated documentation and bug fixes (#282)
    * dummy commit
    
    * Added Remove label functionality and updated documentation and bug fixes
    
    * changed DP service endpoint
    
    * added dummy commit

--- data_labeling_examples/bulk_labeling_python/classification_config.py
+++ data_labeling_examples/bulk_labeling_python/classification_config.py
@@ -3,10 +3,10 @@
 LIST_RECORDS_LIMIT = 1000
 # the algorithm that will be used to assign labels to DLS Dataset records
 # Possible values for labeling algorithm "FIRST_LETTER_MATCH", "FIRST_REGEX_MATCH", "CUSTOM_LABELS_MATCH"
-LABELING_ALGORITHM = "CUSTOM_LABELS_MATCH"
+LABELING_ALGORITHM = "FIRST_LETTER_MATCH"
 # an array where the elements are all of the labels that you will use to annotate records in your DLS Dataset with.
 # Each element is a separate label.
-LABELS = ["dog", "cat"]
+LABELS = ["jpeg", "jpg"]
 
 # use for first_match labeling algorithm
 FIRST_MATCH_REGEX_PATTERN = r'^([^/]*)/.*$'
