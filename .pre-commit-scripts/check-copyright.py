5a6260dade6838267fb8f6c53b42b9ec77d479cc
lrudenka <liuda.rudenka@oracle.com>
2023-06-15 16:51:34 -0500

    ODSC-42791. Add pre-commit hooks for ocids, copyright and secrets
    - added check-copyright.py script
    - added .pre-commit-hooks.yaml
    - added .pre-commit-config.yaml
    - added .gitleaks.toml

---
+++ .pre-commit-scripts/check-copyright.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2023 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
+import datetime
+import os
+import sys
+
+CURRENT_YEAR = datetime.date.today().year
+# At the beginning of next year line has to be added in this list:
+PUBLISHED_LAST_EDITED_YEARS = [
+    f"Copyright (c) 2020, {CURRENT_YEAR} Oracle and/or its affiliates",
+    f"Copyright (c) 2021, {CURRENT_YEAR} Oracle and/or its affiliates",
+    f"Copyright (c) 2022, {CURRENT_YEAR} Oracle and/or its affiliates",
+    f"Copyright (c) {CURRENT_YEAR} Oracle and/or its affiliates",
+]
+LICENSE_STATEMENTS = [
+    "Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/"
+]
+
+def main(filenames) -> int:
+    phrases = LICENSE_STATEMENTS
+    years = PUBLISHED_LAST_EDITED_YEARS
+    retcode = 0
+    for filename in filenames:
+        if not os.path.basename(filename).startswith("."):
+            with open(filename) as inputfile:
+                content = inputfile.read()
+                if not any(x in content for x in years):
+                    print(f"{filename}: Year published or year last edited not correct.")
+                    retcode = 1
+                    break
+                for p in phrases:
+                    if p not in content:
+                        print(f"{filename}: Copyright text missing or incomplete.")
+                        retcode = 1
+                        break
+
+    sys.exit(retcode)
+
+
+if __name__ == "__main__":
+    filenames = sys.argv
+    main(filenames)
