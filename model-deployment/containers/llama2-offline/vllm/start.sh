7d53b93e69137133890579b96c5fcefbb77a7a5b
nipgarg <nipun.garg@oracle.com>
2023-09-14 21:20:05 +0530

    Add params configuration for vllm model to provide config dynamically. Update readme.

--- model-deployment/containers/llama2-offline/vllm/start.sh
+++ model-deployment/containers/llama2-offline/vllm/start.sh
@@ -2,4 +2,4 @@
 
 echo "Starting vllm engine..."
 source activate vllm
-WEB_CONCURRENCY=1 python api-server.py --port ${PORT} --host 0.0.0.0 --log-config /etc/log-config.yaml --model ${MODEL} --tensor-parallel-size ${TENSOR_PARALLELISM}
+WEB_CONCURRENCY=1 python api-server.py --port ${PORT} --host 0.0.0.0 --log-config /etc/log-config.yaml --model ${MODEL} --tensor-parallel-size ${TENSOR_PARALLELISM} ${PARAMS}
