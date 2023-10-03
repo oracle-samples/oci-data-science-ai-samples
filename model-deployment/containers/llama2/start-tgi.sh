56103edb035cf1ed14edcc1a0a69b4a661a6e20b
nipgarg <nipun.garg@oracle.com>
2023-10-03 11:54:38 +0530

    TGI merge

--- model-deployment/containers/llama2/start-tgi.sh
+++ model-deployment/containers/llama2/start-tgi.sh
@@ -1,22 +1,19 @@
 #!/bin/bash 
+if [ -z "$TOKEN_FILE" ] ; then
+  echo "No authentication token is provided. Weights are assumed to be downloaded from OCI Model Catalog."
+else
+  export HUGGING_FACE_HUB_TOKEN=$(cat $TOKEN_FILE)
+  echo "Checking internet connection: "
+  curl -sI -v https://www.wikipedia.org
+  echo "Downloading weights:"
+  text-generation-server download-weights $MODEL
+  echo "Download weights complete"
+  echo $(du -sh /home/datascience/*)
+fi
 
-export HUGGING_FACE_HUB_TOKEN=$(cat $TOKEN_FILE)
-
-echo "Checking internet connection: "
-
-curl -sI -v https://www.wikipedia.org
-# echo "Downloading weights:"
-
-# text-generation-server download-weights $MODEL_ID
-
-# echo "Download complete"
-echo $(du -sh /home/datascience/*)
-
-echo "starting nginx server and TGI"
-nginx -p $PWD && text-generation-launcher --json-output $PARAMS
+echo "Starting TGI..."
+text-generation-launcher --json-output --hostname 0.0.0.0 --port $PORT --model-id $MODEL $PARAMS
 
 echo "Exiting TGI. Here is the disk utilization of /home/datascience - "
 echo $(du -sh /home/datascience)
- 
 exit $LastExitCode
-
