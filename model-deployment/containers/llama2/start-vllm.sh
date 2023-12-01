3b4614d2aa089cbb8d9bcad6dc71371aa4fe6ff3
lyudmil-pelov <lyudmil.x.pelov@oracle.com>
2023-12-01 18:17:37 +0000

    updating vllm deployment

--- model-deployment/containers/llama2/start-vllm.sh
+++ model-deployment/containers/llama2/start-vllm.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-if [ -z "$TOKEN_FILE" ] ; then
-  echo "No authentication token is provided. Weights are assumed to be downloaded from OCI Model Catalog."
+if [[ -z "${MODEL_DEPLOYMENT_OCID}" ]]; then
+  auth_method=instance_principal
 else
+  auth_method=resource_principal
+fi
+
+if [ -n "\$BUCKET" ]; then
+  echo "BUCKET variable are set."
+  #oci os object sync --auth resource_principal --bucket-name genai --dest-dir /home/datascience/llma2/
+  /root/bin/oci os object sync --auth $auth_method --bucket-name $BUCKET --dest-dir /home/datascience/model/
+  MODEL="/home/datascience/model/$MODEL"  
+elif [ -n "\$TOKEN_FILE" ]; then
   export HUGGING_FACE_HUB_TOKEN=$(cat $TOKEN_FILE)
   echo "The md5 of token is $(md5sum $TOKEN_FILE)"
   mkdir -p /home/datascience/.cache/huggingface
@@ -14,13 +23,15 @@ else
   echo $(df -h /home/datascience)
   df -h
   echo "Checking internet connection: "
-  curl -sI -v https://www.wikipedia.org
+  curl -s --connect-timeout 15 http://example.com > /dev/null && echo "Connected" || echo "Not connected"
   echo $(du -sh /home/datascience/*)
+else
+  echo "No bucket or authentication token is provided. Weights are assumed to be downloaded from OCI Model Catalog."
 fi
 
 echo "Starting vllm engine..."
 source activate vllm
-WEB_CONCURRENCY=1 python $VLLM_DIR/vllm-api-server.py --port ${PORT} --host 0.0.0.0 --log-config $VLLM_DIR/vllm-log-config.yaml --model ${MODEL} --tensor-parallel-size ${TENSOR_PARALLELISM} ${PARAMS}
+WEB_CONCURRENCY=1 python $VLLM_DIR/vllm-api-server.py --port ${PORT} --host 0.0.0.0 --log-config $VLLM_DIR/vllm-log-config.yaml --model ${MODEL} --tensor-parallel-size ${TENSOR_PARALLELISM}
 
 
 echo "Exiting vLLM. Here is the disk utilization of /home/datascience - "
