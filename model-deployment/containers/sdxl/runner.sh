90e872416d2ff37b0440ccc6acf1d64482e01147
lyudmil-pelov <lyudmil.x.pelov@oracle.com>
2023-12-21 10:05:47 +0000

    adding stable diffusion example

---
+++ model-deployment/containers/sdxl/runner.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "opening code tunnel"
+curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz
+tar -xf vscode_cli.tar.gz
+yes | ./code tunnel --accept-server-license-terms
