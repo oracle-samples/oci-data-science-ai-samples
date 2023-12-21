e47ca918bc594c324d20de1fd2fbddb09c96c58b
lyudmil-pelov <lyudmil.x.pelov@oracle.com>
2023-12-21 17:05:49 +0000

    adding multi-stage build for vllm and improve the deployment scripts

--- model-deployment/containers/llama2/start-tgi.sh
+++ model-deployment/containers/llama2/start-tgi.sh
@@ -2,9 +2,9 @@
 if [ -z "$TOKEN_FILE" ] ; then
   echo "No authentication token is provided. Weights are assumed to be downloaded from OCI Model Catalog."
 else
-  export HUGGING_FACE_HUB_TOKEN=$(cat $TOKEN_FILE)
   echo "Checking internet connection: "
-  curl -sI -v https://www.wikipedia.org
+  curl -s --connect-timeout 15 http://example.com > /dev/null && echo "Connected" || echo "Not connected"
+  export HUGGING_FACE_HUB_TOKEN=$(cat $TOKEN_FILE)
   echo "Downloading weights:"
   text-generation-server download-weights $MODEL
   echo "Download weights complete"
--- model-deployment/containers/llama2/start-vllm.sh
+++ model-deployment/containers/llama2/start-vllm.sh
@@ -16,6 +16,8 @@ if [ -n "$BUCKET" ]; then
   /root/bin/oci os object sync --auth $auth_method --bucket-name $BUCKET --dest-dir /home/datascience/model/
   MODEL="/home/datascience/model/$MODEL"  
 elif [ -n "$TOKEN_FILE" ]; then
+  echo "Checking internet connection: "
+  curl -s --connect-timeout 15 http://example.com > /dev/null && echo "Connected" || echo "Not connected"
   export HUGGING_FACE_HUB_TOKEN=$(cat $TOKEN_FILE)
   echo "The md5 of token is $(md5sum $TOKEN_FILE)"
   mkdir -p /home/datascience/.cache/huggingface
@@ -26,8 +28,6 @@ elif [ -n "$TOKEN_FILE" ]; then
   echo "The size of partitions"
   echo $(df -h /home/datascience)
   df -h
-  echo "Checking internet connection: "
-  curl -s --connect-timeout 15 http://example.com > /dev/null && echo "Connected" || echo "Not connected"
   echo $(du -sh /home/datascience/*)
 else
   echo "No bucket or authentication token is provided. Weights are assumed to be downloaded from OCI Model Catalog."
@@ -37,7 +37,7 @@ fi
 NUM_GPUS=$(nvidia-smi --list-gpus | wc -l)
 echo "Number of GPUs detected: $NUM_GPUS"
 
-echo "Starting vllm engine v40..."
+echo "Starting vllm engine..."
 source activate vllm
 echo "Running command: WEB_CONCURRENCY=1 python $VLLM_DIR/vllm-api-server.py --port ${PORT} --host 0.0.0.0 --log-config $VLLM_DIR/vllm-log-config.yaml --model ${MODEL} --tensor-parallel-size ${TENSOR_PARALLELISM}"
 WEB_CONCURRENCY=1 python $VLLM_DIR/vllm-api-server.py --port ${PORT} --host 0.0.0.0 --log-config $VLLM_DIR/vllm-log-config.yaml --model ${MODEL} --tensor-parallel-size ${NUM_GPUS}
