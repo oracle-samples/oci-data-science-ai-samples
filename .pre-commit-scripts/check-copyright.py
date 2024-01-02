72c004d8ed8b7ea5454a84faef5c55491f5d9fb3
Allen Hosler <allen.hosler@oracle.com>
2024-01-02 15:01:13 +0000

    Have year adjust automatically

--- .pre-commit-scripts/check-copyright.py
+++ .pre-commit-scripts/check-copyright.py
@@ -10,12 +10,9 @@ import sys
 CURRENT_YEAR = datetime.date.today().year
 
 ### At the beginning of next year line has to be added in this list:
-PUBLISHED_LAST_EDITED_YEARS = [
-    f"Copyright (c) 2020, {CURRENT_YEAR} Oracle and/or its affiliates",
-    f"Copyright (c) 2021, {CURRENT_YEAR} Oracle and/or its affiliates",
-    f"Copyright (c) 2022, {CURRENT_YEAR} Oracle and/or its affiliates",
-    f"Copyright (c) {CURRENT_YEAR} Oracle and/or its affiliates",
-]
+PUBLISHED_LAST_EDITED_YEARS = \
+[f"Copyright (c) {y}, {CURRENT_YEAR} Oracle and/or its affiliates" for y in range(2020, CURRENT_YEAR)] + \
+[f"Copyright (c) {CURRENT_YEAR} Oracle and/or its affiliates"]
 
 LICENSE_STATEMENTS = [
     "Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/"
