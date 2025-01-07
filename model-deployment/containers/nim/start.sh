493ce4537ff4dbc3a576c276d64c0682ee31205e
nipgarg <nipun.garg@oracle.com>
2025-01-07 10:10:42 +0530

    Exit code

--- model-deployment/containers/nim/start.sh
+++ model-deployment/containers/nim/start.sh
@@ -7,6 +7,6 @@ else
   echo "Starting server..."
   # At present, there does not seem to exist any NIM configuration to successfully override max model length, so need to manually configure vLLM command
   WEB_CONCURRENCY=1 python3 -m vllm_nvext.entrypoints.openai.api_server --max-model-len 2048 --enforce-eager --tensor-parallel-size 2
-  exit $LastExitCode
+  exit $?
 fi
 
